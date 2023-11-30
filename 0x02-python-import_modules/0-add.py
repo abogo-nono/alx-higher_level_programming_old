2b1f5e36d7aad795ff74f3fe7ad4fc37d44c54db
Abogo <abogonono1@gmail.com>
2023-11-30 23:34:16 +0100

    a script that import a add function from a file and used it in main prog

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
-s = add(1,2)
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    s = add(1,2)
 
-print(f"{a} + {b} = {s}")
+    print(f"{a} + {b} = {s}")
