9a3d3e90a76ed09bb531193cc7d7f1ce9adf9810
Abogo <abogonono1@gmail.com>
2023-12-01 02:40:13 +0100

    mini calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub,  mul, div
 
-argc = len(sys.argv) - 1
+argc = len(sys.argv)
 
-if argc != 3:
+if argc - 1 != 3:
     print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
     sys.exit(1)
 
-operators = {'+':add, '-':sub, '*':mul, '/':div}
+operators = {'+': add, '-': sub, '*': mul, '/': div}
 
 if sys.argv[2] not in list(operators.keys()):
     print("Unknown operator. Available operators: +, -, * and /")
