f72baecfd3d75a8c32c1851b65aac4ca062fb5a7
Abogo Lincoln <abogonono1@gmail.com>
2024-01-09 21:15:14 -0600

    this function is used to write in a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""write in file
+"""
+
+
+def write_file(filename="", text=""):
+    """Write a content in a file
+
+    Args:
+        filename (str, optional): file name. Defaults to "".
+        text (str, optional): content to write. Defaults to "".
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(text)
+    return len(text)
